01db177f4bca860b429cc433e04e1494b806a669
riyanris <riyanrisky129@gmail.com>
2023-07-04 15:40:44 +0700

    asem

--- ci42.sql
+++ ci42.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2023 at 10:45 AM
+-- Generation Time: Jul 04, 2023 at 10:40 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
@@ -58,11 +58,41 @@ CREATE TABLE `tbl_anggota` (
   `asl_sekolah` varchar(50) NOT NULL,
   `no_tlpn` varchar(16) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `angkatan` int(1) NOT NULL,
+  `angkatan` varchar(64) NOT NULL,
   `jurusan` varchar(5) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_anggota`
+--
+
+INSERT INTO `tbl_anggota` (`id_anggota`, `nama`, `tmpt_lahir`, `tgl_lahir`, `jenis_kelamin`, `agama`, `alamat`, `asl_sekolah`, `no_tlpn`, `email`, `angkatan`, `jurusan`, `username`, `password`) VALUES
+(1, 'Ini nama lengkapppp', 'sleman', '2002-04-14', 'Laki-laki', 'Khatolik', 'Jl Mawar Bunga melati', 'ugm', '628966772323', 'ini@email.com', '2008', 'infor', '1234', '$2y$10$HEI.JkV5xm1k9Tk/P86p.O4gYQTaieuRRbAhfK7cgZlCKiqaSAaUa'),
+(2, 'nama2', '123', '1223-02-12', 'Perempuan', 'Khatolik', '123123', '123123', '12312', '3123@das.22', '3123123', '23232', '12345', '$2y$10$8kzn3b6g8EeaDnPoOtTbX.Ohcux3vz.O49/XgIfTCP/J/411E/pSC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_banner`
+--
+
+CREATE TABLE `tbl_banner` (
+  `id_banner` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `upload_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_banner`
+--
+
+INSERT INTO `tbl_banner` (`id_banner`, `nama`, `file`, `upload_at`) VALUES
+(3, 'Foto Keluarga Besar', '1688448498_6f87f7a98bcc14284038.jpeg', '2023-07-04 12:28:18'),
+(4, 'Bermain Dengan Siapa', '1688448517_3bf9ed601d51b614e24c.jpeg', '2023-07-04 12:28:37'),
+(6, 'Banyak Sekali Kawan', '1688458240_ba84379b6427f70d5552.jpeg', '2023-07-04 15:10:40');
 
 -- --------------------------------------------------------
 
@@ -77,6 +107,14 @@ CREATE TABLE `tbl_berkas` (
   `upload_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_berkas`
+--
+
+INSERT INTO `tbl_berkas` (`id_berkas`, `nama`, `file`, `upload_at`) VALUES
+(4, 'hasil rat 2023', '1688445084_d8da56b0dd6c1a8657dc.pdf', '2023-07-04 11:31:24'),
+(5, 'dokumentasi rapat tahunan ', '1688445101_bbdcedde97657a438337.pdf', '2023-07-04 11:31:41');
+
 -- --------------------------------------------------------
 
 --
@@ -89,6 +127,32 @@ CREATE TABLE `tbl_galeri` (
   `file` varchar(255) NOT NULL,
   `upload_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_galeri`
+--
+
+INSERT INTO `tbl_galeri` (`id_galeri`, `nama`, `file`, `upload_at`) VALUES
+(1, 'Riyan', '1688448306_a779a5bde30ff95765e6.jpeg', '2023-07-04 12:25:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_info`
+--
+
+CREATE TABLE `tbl_info` (
+  `id_info` int(11) NOT NULL,
+  `visi` text NOT NULL,
+  `misi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_info`
+--
+
+INSERT INTO `tbl_info` (`id_info`, `visi`, `misi`) VALUES
+(1, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque voluptatem provident, velit ea dolore voluptatibus? asdasd Voluptate, cum laborum eligendi hic, reprehenderit aliquid temporibus dolor facilis tempora magnam beatae aliquam ipsum.. asdasdas', 'Lorem, ipsum dolor sit amet consectetuasdsar adipisicing elit. Velit corrupti earum, voluptate voluptas vel harum enim tempore corporis! Expedita doloremque reiciendis quae voluptas eius quas esse atque praesentium nisi sed');
 
 -- --------------------------------------------------------
 
@@ -112,6 +176,28 @@ INSERT INTO `tbl_kampus` (`id_kampus`, `nama_kampus`, `alamat_kampus`, `file`, `
 (1, 'stimik akakom yogyakarta', 'Jl. Raya Janti No.143, Jaranan, Banguntapan, Kec. Banguntapan, Bantul, Daerah Istimewa Yogyakarta 55198', '1688372496_d891f031d60cbd3fd619.jpg', '2023-07-03 15:21:36'),
 (2, 'UNIversitas gajah mada', '69HG+CXX, Bulaksumur, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281', '1688373002_15dbeb479e4ecd32d2bd.png', '2023-07-03 15:30:02');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_struktur`
+--
+
+CREATE TABLE `tbl_struktur` (
+  `id_struktur` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jabatan` varchar(64) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `upload_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_struktur`
+--
+
+INSERT INTO `tbl_struktur` (`id_struktur`, `nama`, `jabatan`, `file`, `upload_at`) VALUES
+(1, 'RIyan Ris', 'Ketua Umum', '1688448401_b9f144256ccd9fe0241c.jpeg', '2023-07-04 12:26:41'),
+(2, 'Fhareza Alvindo', 'Pembina', '1688448447_a0840ddda1bf513e852b.jpeg', '2023-07-04 12:27:27');
+
 --
 -- Indexes for dumped tables
 --
@@ -129,6 +215,12 @@ ALTER TABLE `tbl_anggota`
   ADD PRIMARY KEY (`id_anggota`);
 
 --
+-- Indexes for table `tbl_banner`
+--
+ALTER TABLE `tbl_banner`
+  ADD PRIMARY KEY (`id_banner`);
+
+--
 -- Indexes for table `tbl_berkas`
 --
 ALTER TABLE `tbl_berkas`
@@ -141,10 +233,22 @@ ALTER TABLE `tbl_galeri`
   ADD PRIMARY KEY (`id_galeri`);
 
 --
+-- Indexes for table `tbl_info`
+--
+ALTER TABLE `tbl_info`
+  ADD PRIMARY KEY (`id_info`);
+
+--
 -- Indexes for table `tbl_kampus`
 --
 ALTER TABLE `tbl_kampus`
   ADD PRIMARY KEY (`id_kampus`);
+
+--
+-- Indexes for table `tbl_struktur`
+--
+ALTER TABLE `tbl_struktur`
+  ADD PRIMARY KEY (`id_struktur`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -160,25 +264,43 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_anggota`
 --
 ALTER TABLE `tbl_anggota`
-  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_banner`
+--
+ALTER TABLE `tbl_banner`
+  MODIFY `id_banner` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_berkas`
 --
 ALTER TABLE `tbl_berkas`
-  MODIFY `id_berkas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_berkas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_galeri`
 --
 ALTER TABLE `tbl_galeri`
-  MODIFY `id_galeri` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_galeri` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_info`
+--
+ALTER TABLE `tbl_info`
+  MODIFY `id_info` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_kampus`
 --
 ALTER TABLE `tbl_kampus`
   MODIFY `id_kampus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_struktur`
+--
+ALTER TABLE `tbl_struktur`
+  MODIFY `id_struktur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
